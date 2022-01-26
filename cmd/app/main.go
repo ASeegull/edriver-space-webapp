078736e1706e4c640ebb15bf521b4059830f0c2e
Artur Chopikian <artur4ik303@gmail.com>
2022-01-26 17:17:51 +0200

    delete temporary func

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,49 +4,24 @@ import (
 	"github.com/ASeegull/edriver-space-webapp/api/server"
 	"github.com/ASeegull/edriver-space-webapp/config"
 	"github.com/ASeegull/edriver-space-webapp/logger"
-	"github.com/ASeegull/edriver-space-webapp/model"
-	"github.com/gofiber/fiber/v2"
 )
-
-// Temporary plug for handling API-requests.
-func LoginPlug() {
-	app := fiber.New()
-	app.Post("/sign-in", func(c *fiber.Ctx) error {
-		logInData := new(model.SingInData)
-		c.BodyParser(logInData)
-		if logInData.Email == "skskuzan" {
-			if logInData.Password == "oqooq1" {
-				return c.SendString(`{
-					"accesstoken" : "67686ds8676f7fd68766df7",
-					"refreshtoken" : "67686ds8676f7fd68766df7"
-				}`)
-			} else {
-
-				return c.SendString("Wrong Password")
-			}
-		} else {
-			return c.SendString("User doesn't excist")
-		}
-	})
-	app.Listen(":5050")
-}
 
 func main() {
 	// Initializing logger
 	logger.LogInit()
 
 	// Loading config vals
-	config, err := config.LoadConfig("./config")
+	cfg, err := config.LoadConfig("./config")
 	if err != nil {
 		logger.LogErr(err)
 	}
 
 	// Initializing server and passing config to it
-	webapp := server.Init(config)
+	webapp := server.Init(cfg)
 	webapp.BuildRoutes()
 
-	// Starting server
-	go webapp.Start()
-
-	LoginPlug()
+	//Starting server
+	if err := webapp.Start(); err != nil {
+		logger.LogFatal(err)
+	}
 }
